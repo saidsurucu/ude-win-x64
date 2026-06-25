c75c98d77232cd4eb719c38f0464c8e0f580fc9b
saidsurucu <148570570+saidsurucu@users.noreply.github.com>
2026-06-25 21:31:16 +0300

    ICONS: Fluent IconLoaderPatch (Mac'ten birebir kopya; Windows uyarlamasi: YOK - tum imzalar eslesti)

--- scripts/icons/IconLoaderPatch.java
+++ scripts/icons/IconLoaderPatch.java
@@ -1,8 +1,6 @@
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
-import javassist.expr.ExprEditor;
-import javassist.expr.MethodCall;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,9 +11,18 @@ import java.io.FileOutputStream;
  *      BaseMultiResolutionImage'a sarar (Retina keskinlik). KRİTİK.
  *   2) com.alee.global.StyleConstants.disabledIconsTransparency = 0.38f
  *      (WebLaF disabled ikon saydamlığı; orijinal 0.7 monokromda yetersiz). best-effort.
- *   3) org...flamingo...FilteredResizableIcon.paintIcon: ekran-çizimini
- *      AlphaComposite 0.38 ile sarar (Flamingo disabled ikonları; bu sınıf
- *      bu jar'da yalnızca disabled ikonlarda kullanılır). best-effort.
+ *   4) org...flamingo...ImageWrapperIcon.paintIcon: kaynak Image varsa onu
+ *      hedef boyuta bicubic çizen erken-dönüş. Orijinal yol kaynağı 1x
+ *      BufferedImage'e rasterleyip cache'liyordu -> BaseMultiResolutionImage
+ *      kayboluyor, Retina'da tüm Flamingo ikonları (hızlı erişim dahil)
+ *      bulanık/tırtıklı. Doğrudan drawImage AWT'nin @2x varyant seçimini
+ *      korur. best-effort.
+ *   3) org...flamingo...FilteredResizableIcon.paintIcon: gövde tamamen
+ *      değiştirilir -> delegate doğrudan AlphaComposite 0.38 ile çizilir.
+ *      Orijinal yol ikonun 1x rasterini ColorConvertOp(CS_GRAY) ile bozuyordu
+ *      (Retina'da tırtık + ince Fluent çizgilerinde kir); delegate çizimi
+ *      keskinliği ve soluk renkleri korur. Bu sınıf bu jar'da yalnızca
+ *      disabled ikonlarda kullanılır. best-effort.
  *
  * Argümanlar: <editor-app.jar> <out-dir>
  */
@@ -46,8 +53,25 @@ public class IconLoaderPatch {
           + "     $_ = new javax.swing.ImageIcon(__m);"
           + "   }"
           + " } }");
+        // --- 1b) Utils.a(ImageIcon,int,int): getScaledInstance multi-res'i öldürüyor
+        //     (1x bitmap -> Retina'da bloklu; hızlı erişim bu yoldan ölçekleniyor).
+        //     Kaynak BaseMultiResolutionImage ise her varyant ayrı ölçeklenir. ---
+        CtMethod scale = utils.getMethod("a",
+            "(Ljavax/swing/ImageIcon;II)Ljavax/swing/ImageIcon;");
+        scale.insertBefore(
+            "{ if ($1 != null && $2 > 0 && $3 > 0"
+          + "     && $1.getImage() instanceof java.awt.image.BaseMultiResolutionImage) {"
+          + "   java.awt.image.BaseMultiResolutionImage __mr ="
+          + "       (java.awt.image.BaseMultiResolutionImage) $1.getImage();"
+          + "   java.awt.Image __lo = __mr.getResolutionVariant((double)$2, (double)$3);"
+          + "   java.awt.Image __hi = __mr.getResolutionVariant((double)($2*2), (double)($3*2));"
+          + "   java.awt.Image __slo = __lo.getScaledInstance($2, $3, java.awt.Image.SCALE_SMOOTH);"
+          + "   java.awt.Image __shi = __hi.getScaledInstance($2*2, $3*2, java.awt.Image.SCALE_SMOOTH);"
+          + "   return new javax.swing.ImageIcon(new java.awt.image.BaseMultiResolutionImage("
+          + "       new java.awt.Image[]{ __slo, __shi }));"
+          + " } }");
         writeClass(utils, outDir);
-        System.out.println("[IconLoaderPatch] Utils.b multi-resolution yaması yazıldı.");
+        System.out.println("[IconLoaderPatch] Utils.b multi-res + a(ImageIcon,int,int) yamaları yazıldı.");
 
         // --- 2) WebLaF disabled saydamlık 0.7 -> 0.38 (best-effort) ---
         try {
@@ -59,30 +83,42 @@ public class IconLoaderPatch {
             System.out.println("[IconLoaderPatch] UYARI: WebLaF disabled transparency yaması atlandı: " + t);
         }
 
-        // --- 3) Flamingo disabled ikon alpha 0.38 (best-effort) ---
+        // --- 4) Flamingo ImageWrapperIcon: multi-res korunarak çiz (best-effort) ---
+        try {
+            CtClass iwi = pool.get("org.pushingpixels.flamingo.api.common.icon.ImageWrapperIcon");
+            CtMethod paint = iwi.getMethod("paintIcon", "(Ljava/awt/Component;Ljava/awt/Graphics;II)V");
+            paint.insertBefore(
+                "{ if (this.image != null && this.width > 0 && this.height > 0) {"
+              + "    java.awt.Graphics2D g2 = (java.awt.Graphics2D) $2.create();"
+              + "    g2.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION,"
+              + "        java.awt.RenderingHints.VALUE_INTERPOLATION_BICUBIC);"
+              + "    g2.setRenderingHint(java.awt.RenderingHints.KEY_RENDERING,"
+              + "        java.awt.RenderingHints.VALUE_RENDER_QUALITY);"
+              + "    g2.drawImage(this.image, $3, $4, this.width, this.height,"
+              + "        (java.awt.image.ImageObserver) null);"
+              + "    g2.dispose();"
+              + "    return;"
+              + "  } }");
+            writeClass(iwi, outDir);
+            System.out.println("[IconLoaderPatch] Flamingo ImageWrapperIcon multi-res yaması uygulandı.");
+        } catch (Throwable t) {
+            System.out.println("[IconLoaderPatch] UYARI: ImageWrapperIcon yaması atlandı: " + t);
+        }
+
+        // --- 3) Flamingo disabled ikon: delegate + alpha 0.38 (best-effort) ---
         try {
             CtClass fri = pool.get("org.pushingpixels.flamingo.api.common.icon.FilteredResizableIcon");
             CtMethod paint = fri.getMethod("paintIcon", "(Ljava/awt/Component;Ljava/awt/Graphics;II)V");
-            paint.instrument(new ExprEditor() {
-                public void edit(MethodCall m) throws javassist.CannotCompileException {
-                    if (m.getMethodName().equals("drawImage")
-                            && m.getSignature().equals("(Ljava/awt/Image;IILjava/awt/image/ImageObserver;)Z")) {
-                        m.replace(
-                            "{ if ($0 instanceof java.awt.Graphics2D) {"
-                          + "    java.awt.Graphics2D g2 = (java.awt.Graphics2D)$0;"
-                          + "    java.awt.Composite __c = g2.getComposite();"
-                          + "    g2.setComposite(java.awt.AlphaComposite.getInstance("
-                          + "        java.awt.AlphaComposite.SRC_OVER, 0.38f));"
-                          + "    $_ = $proceed($$);"
-                          + "    g2.setComposite(__c);"
-                          + "  } else { $_ = $proceed($$); } }");
-                    }
-                }
-            });
+            paint.setBody(
+                "{ java.awt.Graphics2D g2 = (java.awt.Graphics2D) $2.create();"
+              + "  g2.setComposite(java.awt.AlphaComposite.getInstance("
+              + "      java.awt.AlphaComposite.SRC_OVER, 0.38f));"
+              + "  this.delegate.paintIcon($1, g2, $3, $4);"
+              + "  g2.dispose(); }");
             writeClass(fri, outDir);
-            System.out.println("[IconLoaderPatch] Flamingo disabled alpha yaması uygulandı (0.38).");
+            System.out.println("[IconLoaderPatch] Flamingo disabled: delegate+alpha 0.38 yaması uygulandı.");
         } catch (Throwable t) {
-            System.out.println("[IconLoaderPatch] UYARI: Flamingo disabled alpha yaması atlandı: " + t);
+            System.out.println("[IconLoaderPatch] UYARI: Flamingo disabled yaması atlandı: " + t);
         }
     }
 
